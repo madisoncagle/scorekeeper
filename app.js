f0c7ab0f2ce3d0ce91e37c044632695bf38025ea
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-11-24 14:11:30 -0600

    Finish modal UI

--- app.js
+++ app.js
@@ -51,21 +51,23 @@ for (let card of playerCards) {
 }
 
 // modal control
-var modal = document.getElementById("add-player-modal");
-var btn = document.getElementById("add-player-btn");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("add-player-modal");
+let btn = document.getElementById("add-player-btn");
+let x = document.getElementsByClassName("close")[0];
+let submit = document.getElementById("submit-player-name");
 
-// When the user clicks the button, open the modal 
+submit.addEventListener("click", () => {
+    // get name from input
+});
+
 btn.addEventListener("click", () => {
     modal.style.display = "block";
 });
 
-// When the user clicks on <span> (x), close the modal
-span.addEventListener("click", () => {
+x.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// When the user clicks anywhere outside of the modal, close it
 window.addEventListener("click", (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
